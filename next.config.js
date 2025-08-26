9c746c29f9fed63cf148ecd656a6549593444902
Sambhav Barik <63142699+Sambhav-18066@users.noreply.github.com>
2025-08-26 09:56:17 +0530

    delete

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // This app is fully client-side; no special config required.
-};
-module.exports = nextConfig;
